a3c0b3ed461ee8b0a5e21d0b912c7c74f86784b8
Kirtap98 <49925184+Kirtap98@users.noreply.github.com>
2019-04-25 22:44:33 -0300

    Ventana
    Ya agregue la ventana y un poco de nodos

---
+++ Assets/Scripts/ContextMenuexample.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; 
+
+public class ContextMenuexample : MonoBehaviour
+{
+    #region NO REQUIERE IMPORTAR UNITY EDITOR
+
+    public string _myName;
+
+    //añade un context menu especifico a la VARIABLE. pide el texto a mostrar,
+    //seguido del nombre de la funcion a ejecutar
+    [ContextMenuItem("RandomHP", "Randomize")]
+    public int myHp;
+
+    //agrega este elemento al menu contextual del COMPONENTE, para ejecutar esta funcion
+    [ContextMenu("Reset name value")]
+    private void ResetName()
+    {
+        _myName = "";
+    }
+
+    private void Randomize()
+    {
+        myHp = Random.Range(0, 100);
+    }
+    #endregion
+
+    #region REQUIERE UNITY_EDITOR
+
+    [MenuItem("CONTEXT/Transform/ResetAll")]
+    private static void MyTransformOption(MenuCommand menuCommand)
+    {
+        var tr = menuCommand.context as Transform; //consigo referencia al transform clikeado
+        tr.position = Vector3.one;
+        tr.rotation = Quaternion.identity;
+        tr.localScale = Vector3.one;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Nodes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nodes : MonoBehaviour
+{
+    public Rect myRect;
+    public string nodeName;
+    public string dialogo;
+    public float duration;
+    private bool _overNode;
+    public List<Nodes> connected;
+
+    public Nodes(float x, float y, float width, float height, string name)
+    {
+        myRect = new Rect(x, y, width, height);
+        connected = new List<Nodes>();
+        nodeName = name;
+    }
+
+    public void CheckMouse(Event cE, Vector2 pan)
+    {
+        _overNode = myRect.Contains(cE.mousePosition - pan);
+    }
+
+    public bool OverNode
+    { get { return _overNode; } }
+}
