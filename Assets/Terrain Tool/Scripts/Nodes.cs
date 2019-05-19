a1b6fb4e9fc66831b87e352b5390eca1232d5c06
Manuelsi <16804326+Manuelsi@users.noreply.github.com>
2019-05-19 20:47:10 -0300

    Nodos cargables y seteables
    Agregado metodos para poder guardar y cargar los nodos con una estructura serializable que contiene la data. Permite guardar y cargar informacion

--- Assets/Terrain Tool/Scripts/Nodes.cs
+++ Assets/Terrain Tool/Scripts/Nodes.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -8,20 +9,19 @@ public abstract class DrawnNode
     public string nodeName;
 	public DrawnNode nextNode;
 
-	public abstract string Content { get; }
-	public abstract string NodeType { get; }
+	public abstract string Content { get; protected set; }
+	public abstract string NodeType { get; protected set; }
+	protected abstract Vector2 WindowSize { get; }
 
-	private Rect _myRect;
+	protected Rect _myRect;
     public Rect MyRect {
 		get => _myRect;
-		private set => _myRect = value;
+		protected set => _myRect = value;
 	}
 
 	public Vector2 RectPos {
 		get => MyRect.position;
-		set {
-			_myRect.position = value;
-		}
+		set => _myRect.position = value ;
 	}
 
 	public virtual Vector2 ArrowTargetPos {
@@ -32,10 +32,14 @@ public abstract class DrawnNode
 		get => new Vector2(RectPos.x + MyRect.width, MyRect.y + (MyRect.height / 2f));
 	}
 
-    public DrawnNode(Rect rect)
+    public DrawnNode(Vector2 position)
     {
-		MyRect = rect;
+		_myRect = new Rect(position, WindowSize);
     }
+
+	public DrawnNode() {
+		_myRect = new Rect(0, 0, WindowSize.x, WindowSize.y);
+	}
 
     public bool CheckMouse(Event cE, Vector2 pan)=>
         MyRect.Contains(cE.mousePosition - pan);
@@ -46,5 +50,28 @@ public abstract class DrawnNode
     //public bool OverNode
     //{ get { return _overNode; } }
 
+	public virtual NodeData GetData() {
+		var data = new NodeData();
+		data.nodeType = NodeType;
+		data.data = new List<object> {
+			nodeName, //0
+			Content //1
+		};
+		return data;
+	}
 
+	public virtual void SetData(NodeData data) {
+		if(NodeType != data.nodeType)
+			//throw new ArgumentException($"Node type does not match. Expected {NodeType} but got {data.nodeType}.");
+			throw new ArgumentException($"Tipos de nodos no coinciden. Esperaba {NodeType} pero obtuvo {data.nodeType}.");
+		var info = data.data;
+		nodeName = (string)info[0];
+		Content = (string)info[1];
+	}
+}
+
+[Serializable]
+public struct NodeData {
+	public string nodeType;
+	public List<object> data;
 }
